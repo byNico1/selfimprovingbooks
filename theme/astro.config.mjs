947d02734643703aec737b4a64e51e9ea14bda51
bynico1 <nicofoque0205@gmail.com>
2023-08-16 11:00:20 -0500

    build proof

--- theme/astro.config.mjs
+++ theme/astro.config.mjs
@@ -13,7 +13,7 @@ export default defineConfig({
   // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
   sitemap: true,
   // Generate sitemap (set to "false" to disable)
-  integrations: [sitemap(), mdx(), image(), react(), tailwind()],
+  integrations: [sitemap(), mdx(), lit(), image(), react(), tailwind()],
   // Add renderers to the config
   // This is for the astro-icon package. You can find more about the package here: https://www.npmjs.com/package/astro-icon
   vite: {
