242d08c13286037b77a2ccbb813b350197964e6c
bynico1 <nicofoque0205@gmail.com>
2023-08-16 10:21:16 -0500

    First commit

--- theme/astro.config.mjs
+++ theme/astro.config.mjs
@@ -1,16 +1,20 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 import mdx from "@astrojs/mdx";
 import image from "@astrojs/image";
 import lit from "@astrojs/lit";
+import react from "@astrojs/react";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://odyssey-theme.littlesticks.dev/',
+  site: "https://odyssey-theme.littlesticks.dev/",
   // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
   sitemap: true,
   // Generate sitemap (set to "false" to disable)
-  integrations: [sitemap(), mdx(), image(), lit()], // Add renderers to the config
+  integrations: [sitemap(), mdx(), image(), react(), tailwind()],
+  // Add renderers to the config
   // This is for the astro-icon package. You can find more about the package here: https://www.npmjs.com/package/astro-icon
   vite: {
     ssr: {
--- theme/src/config/nav.js
+++ theme/src/config/nav.js
@@ -1,18 +1,39 @@
 export const nav = [
-	{
-		title: 'Home',
-		slug: '/',
-	},
-	{
-		title: 'Blog',
-		slug: '/blog',
-	},
-	{
-		title: 'About',
-		slug: '/company/about',
-	},
-	{
-		title: 'Contact',
-		slug: '/company/contact',
-	},
+  {
+    title: "Home",
+    slug: "/",
+    dropdown: false,
+  },
+  {
+    title: "Categories",
+    slug: "/categories",
+    subcat: [
+      { title: "Biographies & Memoirs", slug: "/categories/bio-memoirs" },
+      { title: "Business & Money", slug: "/categories/business-money" },
+      { title: "Self-Help", slug: "/categories/self-help" },
+      { title: "All", slug: "/categories/all" },
+    ],
+    // subcatslug: ["/bio-memoirs", "/business-money", "/self-help"],
+    dropdown: true,
+  },
+  {
+    title: "Best Sellers",
+    slug: "/best-sellers",
+    dropdown: false,
+  },
+  {
+    title: "Deals and Discounts",
+    slug: "/deals",
+    dropdown: false,
+  },
+  {
+    title: "Community",
+    slug: "/community",
+    subcat: [
+      { title: "Books reviews", slug: "/community/books-reviews" },
+      { title: "Blog", slug: "/blog" },
+      { title: "About us", slug: "/community/about-us" },
+    ],
+    dropdown: true,
+  },
 ];
