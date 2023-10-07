7874d735006840d9baea6b41395b49461e291f9b
bynico1 <nicofoque0205@gmail.com>
2023-10-06 19:57:28 -0500

    Trying to fix robots not indexing bug

--- theme/astro.config.mjs
+++ theme/astro.config.mjs
@@ -3,8 +3,9 @@ import sitemap from "@astrojs/sitemap";
 import mdx from "@astrojs/mdx";
 import lit from "@astrojs/lit";
 import react from "@astrojs/react";
-
 import tailwind from "@astrojs/tailwind";
+
+import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
 export default defineConfig({
@@ -12,16 +13,18 @@ export default defineConfig({
   // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
   sitemap: true,
   // Generate sitemap (set to "false" to disable)
-  integrations: [sitemap(), mdx(), lit(), react(), tailwind()],
+  integrations: [sitemap(), mdx(), lit(), react(), tailwind(), robotsTxt()],
   // Add renderers to the config
   // This is for the astro-icon package. You can find more about the package here: https://www.npmjs.com/package/astro-icon
   vite: {
     ssr: {
-      external: ["svgo"],
-    },
+      external: ["svgo"]
+    }
   },
   image: {
-    remotePatterns: [{ protocol: "https" }],
-    domains: ["jamesclear.com", "https://amzn.to", "https://amazon.com"],
-  },
+    remotePatterns: [{
+      protocol: "https"
+    }],
+    domains: ["jamesclear.com", "https://amzn.to", "https://amazon.com"]
+  }
 });
