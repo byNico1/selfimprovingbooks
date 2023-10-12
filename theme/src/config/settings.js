02de18a4c1b10bf981f40d7e7c51f7cd6c20c0f7
bynico1 <nicofoque0205@gmail.com>
2023-10-12 14:03:45 -0500

    canonical .com

--- theme/src/config/settings.js
+++ theme/src/config/settings.js
@@ -1,7 +1,7 @@
 export default {
   title: `Self Improvement Books: Top Picks for Personal Growth`,
   description: `Explore the best self-improvement books to boost your personal growth journey. Find inspiration, motivation, and valuable insights within these pages.`,
-  url: `https://selfimprovingbooks.com`, // No trailing slash!
+  url: `https://www.selfimprovingbooks.com`, // No trailing slash!
   name: `SIB`, // The short name of the business or brand name. Used for things like the copyright in the footer.
   enableThemeSwitcher: false,
   showLittleSticksPlug: false, // Disable this if you want to remove the Little Sticks plug from the footer. (╯°□°)╯︵ ┻━┻
